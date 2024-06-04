38ca6ab0df893736141889656ca1738e3ae59b1f
Nico98Ar <rakcin785@gmail.com>
2024-06-04 12:17:50 -0500

    users

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ app = FastAPI()
 
 template = Jinja2Templates(directory="./view")
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def root(req: Request):
     return template.TemplateResponse("index.html",{"request": req})
+@app.get("/signup", response_class=HTMLResponse)
+def sigunp(req: Request):
+    return template.TemplateResponse("signup.html", {"request":req})
+@app.get("user", response_class=HTMLResponse)
+def user(req: Request):
+    return template.TemplateResponse("user.html", {"request":req})
+    
